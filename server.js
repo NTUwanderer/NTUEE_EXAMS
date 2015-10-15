05b3ce467afe6eb9175e6e8bd03b11b9d0e94b02
YourName <YourEmail>
2015-10-15 12:56:17 +0800

    someDB

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
-var app = express();
+var app = express.createServer();
+var io = require('socket.io')(app);
 
 app.listen(8000, function(){
     console.log('Listening on localhost: 8000');
 })
 
 app.use(express.static(__dirname + "/"));
+
+
