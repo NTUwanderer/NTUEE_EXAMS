fb109c9d54afc4ff15605cd808dda94e51e0ee49
NTUwanderer <harveyycyang@gmail.com>
2015-10-31 17:05:46 +0800

    examMap.js edited...

--- database/examMap.js
+++ database/examMap.js
@@ -1,46 +1,22 @@
 module.exports = {
-  calculus1 : {
-    quiz : [],
-    exam : []
-  },
-  calculus2 : {
-    quiz : [],
-    exam : []
-  },
-  physics1 : {
-    quiz : [],
-    exam : []
-  },
-  physics2 : {
-    quiz : [],
-    exam : []
-  },
   cprogram : {
-    quiz : [],
-    exam : []
-  },
-  biology : {
-    quiz : [],
-    exam : []
-  },
-  chemistry : {
-    quiz : [],
-    exam : []
+    quiz : ["第一次小考"],
+    exam : ["期中考", "期末考"]
   },
   cscience : {
     quiz : [],
-    exam : []
+    exam : ["期中考", "期末考"]
   },
   linear : {
-    quiz : [],
-    exam : []
+    quiz : ["第一次小考", "第二次小考"],
+    exam : ["期中考", "期末考"]
   },
   electron1 : {
-    quiz : [],
+    quiz : ["第一次小考", "第二次小考"],
     exam : ["期中考", "期末考"]
   },
   electron2 : {
-    quiz : [],
+    quiz : ["第一次小考", "第二次小考", "第三次小考"],
     exam : ["期中考", "期末考"]
   },
   electron3 : {
@@ -48,40 +24,114 @@ module.exports = {
     exam : ["期中考", "期末考"]
   },
   circuit : {
-    quiz : ["第,一次小考", "第二次小考", "第三次小考", "第四次小考", "第五次小考"]
+    quiz : ["第一次小考", "第二次小考", "第三次小考", "第四次小考", "第五次小考"]
     exam : ["期末考"]
   },
   diff : {
-    quiz : [],
+    quiz : ["第一次小考", "第二次小考"],
     exam : ["期中考", "期末考"]
   },
   logic : {
-    quiz : [],
+    quiz : ["第一次小考", "第二次小考"],
     exam : ["期中考", "期末考"]
   },
   electrom1 : {
     quiz : [],
-    exam : []
+    exam : ["期中考", "期末考"]
   },
-  electrom2 : {
-    quiz : [],
-    exam : []
-  },
+  
   signal : {
     quiz : [],
-    exam : []
+    exam : ["期中考", "期末考"]
   },
   probability : {
     quiz : [],
-    exam : []
+    exam : ["期中考", "期末考"]
   },
   complex : {
-    quiz : [],
-    exam : []
+    quiz : ["第一次小考", "第二次小考", "第三次小考"],
+    exam : ["期中考", "期末考"]
   },
   discrete : {
     quiz : [],
-    exam : []
+    exam : ["期中考", "期末考"]
+  },
+  electrom2 : {
+    quiz : [],
+    exam : ["期中考", "期末考"]
+  },
+  electrom2 : {
+    quiz : ["第一次小考", "第二次小考"],
+    exam : ["期中考", "期末考"]
+  },
+  electron3 : {
+    quiz : [],
+    exam : ["期中考", "期末考"]
+  },
+  eexp1 : {
+    quiz : [],
+    exam : ["期末考"]
+  },
+  eexp3 : {
+    quiz : [],
+    exam : ["期末考"]
+  },
+// Minor
+  algorithm : {
+    quiz : [],
+    exam : ["第一次期中考", "第二次期中考", "第二次期中考"]
+  },
+  bioEng : {
+    quiz : [],
+    exam : ["期中考"]
+  },
+  carch : {
+    quiz : [],
+    exam : ["期中考", "期末考"]
+  },
+  cnetwork : {
+    quiz : [],
+    exam : ["期中考", "期末考"]
+  },
+  control : {
+    quiz : [],
+    exam : ["期中考", "期末考"]
+  },
+  DSnP : {
+    quiz : [],
+    exam : ["期中考", "期末考"]
+  },
+  LS : {
+    quiz : [],
+    exam : ["期中考"]
+  },
+  microEng : {
+    quiz : [],
+    exam : ["期中考"]
+  },
+  mPhy : {
+    quiz : [],
+    exam : ["期末考"]
+  },
+  OOP : {
+    quiz : [],
+    exam : ["期中考"]
+  },
+  PC : {
+    quiz : [],
+    exam : ["期中考", "期末考"]
+  },
+  powerSys : {
+    quiz : [],
+    exam : ["期中考"]
+  },
+  SCTech : {
+    quiz : [],
+    exam : ["期中考"]
+  },
+  VLSI : {
+    quiz : [],
+    exam : ["期中考", "期末考"]
   }
 }
 /*
